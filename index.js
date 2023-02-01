14e0ab19f47c15978e19762a69723b9efe1ebc4e
drake bishop <drakemx85@gmail.com>
2023-02-01 16:45:44 -0600

    wrote integer functions and created random integers in a range between 0 and 10 using the Math object

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = (31);
+let num2 = (2);
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = (8);
+const num4 = (2);
+
+const mod = (4);
+const divide = num3 % num4
+
+const max = Math.max(15, 16, 17, 18, 19, 20);
